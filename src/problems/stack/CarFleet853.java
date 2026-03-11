73cf966c773f2ec7711514c4befc4b82913e270d
lukabrcic <lbrcic4219rn@raf.edu.rs>
2026-03-11 23:01:37 +0100

    implement carFleet method in CarFleet853 class

--- src/problems/stack/CarFleet853.java
+++ src/problems/stack/CarFleet853.java
@@ -1,7 +1,29 @@
 package problems.stack;
 
-public class CarFleet853 {
-    public static int carFleet(int target, int[] position, int[] speed) {
-    }
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Stack;
 
+public class CarFleet853 {
+    public int carFleet(int target, int[] position, int[] speed) {
+        List<int[]> posSpeed = new ArrayList<>();
+
+        for (int i = 0; i < position.length; i++) {
+            posSpeed.add(new int[]{position[i], speed[i]});
+        }
+
+        posSpeed.sort(Comparator.comparingInt(value -> value[0]));
+
+        Stack<Double> fleets = new Stack<>();
+
+        for (int i = position.length - 1; i >= 0; i--) {
+            Double currSpeed = 1.0 * (target - posSpeed.get(i)[0]) / posSpeed.get(i)[1];
+            if(fleets.empty() || fleets.peek() < currSpeed) {
+                fleets.push(currSpeed);
+            }
+        }
+
+        return fleets.size();
+    }
 }
