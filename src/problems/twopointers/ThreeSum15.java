8af55feadc0e7b2a9e2fe6a01315a67eccd78b44
lukabrcic <lbrcic4219rn@raf.edu.rs>
2026-03-16 21:47:15 +0100

    implement threeSum method in ThreeSum15 class

--- src/problems/twopointers/ThreeSum15.java
+++ src/problems/twopointers/ThreeSum15.java
@@ -4,6 +4,31 @@ import java.util.*;
 
 public class ThreeSum15 {
     public List<List<Integer>> threeSum(int[] nums) {
-        return null;
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) continue;
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // duplicates
+
+            int l = i + 1;
+            int r = nums.length - 1;
+
+            while (l < r) {
+                int sum = nums[l] + nums[r] + nums[i];
+                if (sum == 0) {
+                    res.add(List.of(nums[i], nums[l], nums[r]));
+                    l++;
+                    r--;
+                    while(l < r && nums[l] == nums[l - 1]) l++;
+                    continue;
+                }
+                if (sum < 0) {
+                   l++;
+                   continue;
+                }
+                r--;
+            }
+        }
+        return res;
     }
 }
