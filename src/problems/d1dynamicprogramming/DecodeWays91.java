3b751b32f80d1bc37e28bbb7e1cc885c202a4b8b
lukabrcic <lbrcic4219rn@raf.edu.rs>
2026-05-10 19:06:47 +0200

    add implementation for decoding ways problem

---
+++ src/problems/d1dynamicprogramming/DecodeWays91.java
@@ -0,0 +1,22 @@
+package problems.d1dynamicprogramming;
+
+public class DecodeWays91 {
+    public int numDecodings(String s) {
+        int dp, dp2 = 0;
+        int dp1 = 1;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                dp = 0;
+            } else {
+                dp = dp1;
+                if (i + 1 < s.length() && (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7'))
+                    dp += dp2;
+            }
+            dp2 = dp1;
+            dp1 = dp;
+        }
+        return dp1;
+    }
+
+}
